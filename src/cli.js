10212570abea46ca5206f4b5d5cb30f200f2da3a
Robert Jackson <rjackson@linkedin.com>
2017-06-07 15:52:48 -0400

    Refactor to subcommand style.

--- src/cli.js
+++ src/cli.js
@@ -1,30 +1,40 @@
 #!/usr/bin/env node
 'use strict';
 
+const fs = require('fs');
 const name = require('../package').name;
-const argv = require('yargs')
-      .help()
-      .usage(`${name} <options>`)
-      .option('path', { type: 'string', describe: 'directory to process' })
-      .option('mangle', { type: 'boolean', default: true, describe: 'hashes strings to be unrecognized' })
-      .option('output-path', { type: 'string', describe: 'write JSON file of string counts' })
-      .default('path', () => process.cwd(), '.')
-      .argv;
-
 const StringCollector = require('./index');
 
-const fs = require('fs');
+require('yargs')
 
-let collector = new StringCollector({
-  path: argv.path,
-  mangle: argv.mangle
-});
 
-collector.populate();
+  .command({
+    command: 'gather',
+    aliases: ['*'],
+    desc: 'gather strings',
+    builder(yargs) {
+      return yargs
+        .usage(`${name} <options>`)
+        .option('path', { type: 'string', describe: 'directory to process' })
+        .option('mangle', { type: 'boolean', default: true, describe: 'hashes strings to be unrecognized' })
+        .option('output-path', { type: 'string', describe: 'write JSON file of string counts' })
+        .default('path', () => process.cwd(), '.');
+    },
+    handler(argv) {
+      let collector = new StringCollector({
+        path: argv.path,
+        mangle: argv.mangle
+      });
 
-let jsonOutput = JSON.stringify(collector.strings, null, 2);
-if (argv.outputPath) {
-  fs.writeFileSync(argv.outputPath, jsonOutput, { encoding: 'utf-8' });
-} else {
-  console.log(jsonOutput); // eslint-disable-line no-console
-}
+      collector.populate();
+
+      let jsonOutput = JSON.stringify(collector.strings, null, 2);
+      if (argv.outputPath) {
+        fs.writeFileSync(argv.outputPath, jsonOutput, { encoding: 'utf-8' });
+      } else {
+        console.log(jsonOutput); // eslint-disable-line no-console
+      }
+    }
+  })
+  .help()
+  .argv;
